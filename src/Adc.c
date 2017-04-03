5b2b675713d0cedcb2506e008a8d5292ae2dbb6d
KasparsD <kasparsd@mikrotik.com>
2017-04-03 13:58:25 +0300

    adc

--- src/Adc.c
+++ src/Adc.c
@@ -315,41 +315,41 @@ inline	void ADC_ExternLevel	(void)
 
 	if (val > ext_1.max)
 	{
-		Ext_1.max = val;
+//		Ext_1.max = val;
 		ext_1.max = val;
-//		if ((val > ext_1.center) &&	(ext_1.min < ext_1.center)	)
-//		{
-//			if (Ext_1.center)
-//			{
-//				Ext_1.center -= ext_1.center;
-//				Ext_1.center += (ext_1.max + ext_1.min);
-//			}
-//			else
-//				Ext_1.center  = ((ext_1.max + ext_1.min) << 3);
-//			ext_1.center  = (Ext_1.center >> 4);
-//			Ext_1.center -= ext_1.center;
-//
-//			Ext_1.min	= (Ext_1.min)?	(Ext_1.min + ext_1.min) : (ext_1.min << 4);
-//			Ext_1.min  -= (Ext_1.min >> 4);
-//			ext_1.min	=  ext_1.max;
-//			
-//			val = GetTimeSinceMs(ext_1.time);
-//			Ext_1.time	= (Ext_1.time)?	(Ext_1.time + val) : (val << 4);
-//			Ext_1.time -= (Ext_1.time >> 4);
-//			ext_1.time	= GetTicsMs();
-//		}
+		if ((val > ext_1.center) &&	(ext_1.min < ext_1.center)	)
+		{
+			if (Ext_1.center)
+			{
+				Ext_1.center -= ext_1.center;
+				Ext_1.center += (ext_1.max + ext_1.min);
+			}
+			else
+				Ext_1.center  = ((ext_1.max + ext_1.min) << 3);
+			ext_1.center  = (Ext_1.center >> 4);
+			Ext_1.center -= ext_1.center;
+
+			Ext_1.min	= (Ext_1.min)?	(Ext_1.min + ext_1.min) : (ext_1.min << 4);
+			Ext_1.min  -= (Ext_1.min >> 4);
+			ext_1.min	=  ext_1.max;
+			
+			val = GetTimeSinceMs(ext_1.time);
+			Ext_1.time	= (Ext_1.time)?	(Ext_1.time + val) : (val << 4);
+			Ext_1.time -= (Ext_1.time >> 4);
+			ext_1.time	= GetTicsMs();
+		}
 	}
 	if (val < ext_1.min)
 	{
-		Ext_1.min = val;
+//		Ext_1.min = val;
 		ext_1.min = val;
-//		if ((val < ext_1.center) &&
-//			(ext_1.max > ext_1.center))
-//		{
-//			Ext_1.max	= (Ext_1.max)?	(Ext_1.max + ext_1.max) : (ext_1.max << 4);
-//			Ext_1.max  -= (Ext_1.max >> 4);
-//			ext_1.max	=  ext_1.min;
-//		}
+		if ((val < ext_1.center) &&
+			(ext_1.max > ext_1.center))
+		{
+			Ext_1.max	= (Ext_1.max)?	(Ext_1.max + ext_1.max) : (ext_1.max << 4);
+			Ext_1.max  -= (Ext_1.max >> 4);
+			ext_1.max	=  ext_1.min;
+		}
 	}
 
 
