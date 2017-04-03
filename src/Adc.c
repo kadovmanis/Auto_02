720bd26fd950aee2a93f0a4e73c7a5267cf10081
KasparsD <kasparsd@mikrotik.com>
2017-04-03 15:43:26 +0300

    adc

--- src/Adc.c
+++ src/Adc.c
@@ -358,6 +358,7 @@ inline	void ADC_ExternLevel	(void)
 			if (!--ext_1.center)
 			{
 				Ext_1.max = ext_1.max;
+				ext_1.max	= 0;
 			}
 //			Ext_1.max = ext_1.max;
 //			ext_1.max -= (ext_1.max - ext_1.center);
@@ -396,8 +397,8 @@ inline	void ADC_ExternLevel	(void)
 //		Ext_1.max = ext_1.max;
 		Ext_1.center = (Ext_1.min + Ext_1.max) >> 1;
 //		ext_1.min	= 0xFFFF;
-		ext_1.max	= 0;
-		ext_1.center = 0;
+//		ext_1.max	= 0;
+//		ext_1.center = 0;
 		
 		//		if (!Ext_1.center)
 //		{
